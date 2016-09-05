261774f2236b8a218e6c27df3372500dd2e66423
Eric Bailey <eric.w.bailey@gmail.com>
2016-09-05 16:22:04 -0400

    Updated contributing rules
    https://gitmagic.io/rules/

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,32 @@ var requireDir = require('require-dir');
 
 // Require all tasks in `tasks/`
 requireDir('./tasks', { recurse: true });
+
+
+
+
+
+var paths = {
+    pageLayouts: {
+        input: 'src/layouts/**/{*.html,*shtml}',
+        testing: 'test/_layouts/',
+        watch: 'src/layouts/**/{*.html,*shtml}',
+        dist: 'dist/'
+    },
+    pages: {
+        input: 'src/pages/**/*',
+        exclude: 'src/pages/{_partials,_partials/**}',
+        testing: 'test/',
+        watch: 'src/pages/**/*.html',
+        site: 'test/_site/**/*html',
+        deploy: 'dist/'
+    },
+    sitemap: {
+        input: 'test/_site/sitemap.xml',
+        output: 'dist/'
+    },
+    icons: {
+        input: 'src/touch_icons/{*.ico,*.png}',
+        output: 'dist/'
+    }
+};
