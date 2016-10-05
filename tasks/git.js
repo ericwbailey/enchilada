cae10e5ada868b4ec3ac2d54bfe54c827613ec7b
Eric Bailey <eric.w.bailey@gmail.com>
2016-10-05 13:33:10 -0400

    Git functions match other function styling

--- tasks/git.js
+++ tasks/git.js
@@ -11,17 +11,17 @@ var gulp          = require('gulp-help')(require('gulp')), // http://gulpjs.com/
 
 
 // Tasks ----------------------------------------------------------------------
-gulp.task('git-add', false, function(){
+gulp.task('git-add', false, function() {
     return gulp.src('./docs/*')
         .pipe(git.add());
 });
 
-gulp.task('git-commit', false, function(){
+gulp.task('git-commit', false, function() {
     return gulp.src('./docs/*')
         .pipe(git.commit('Deployed site on ' + currentTime));
 });
 
-gulp.task('git-push', false, function(){
+gulp.task('git-push', false, function() {
     git.push('origin', 'master', function (err) {
         if (err) throw err;
     });
